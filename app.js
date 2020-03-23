cf450b267bb3084b0d30004d49d08453ac0d5993
RicksLWilliams <rlwilliamsix@yahoo.com>
2020-03-23 16:33:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,681 @@
+class Alligator {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Beaver {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Cow {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Deer {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Eagle {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Falcon {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Giraffe {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Hyena {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Iguana {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Jaguar {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Kangaroo {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Leopard {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Meerkat {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Newt {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Okapi {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Penguin {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Quail {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Racoon {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Sloth {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Tiger {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Uguisu {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Vulture {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Warthog {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class XBeaver {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Yak {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+class Zebra {
+  constructor(
+      name = '',
+      color = '',
+      gender = '',
+      weight = 0,
+      tailLengthInch = 0,
+      teethSize = '',
+  ) {
+      this.name = name;
+      this.color = color;
+      this.gender = gender;
+      this.weight = weight;
+      this.tailLengthInch = tailLengthInch;
+      this.teethSize = teethSize;
+  }
+  eat(food) {
+      console.log(`Chomp chomp all the ${food}ies.`)
+  }
+  speak(noise) {
+      alert(`The ${this.constructor.name} says ${noise}!`)
+  }
+}
+
+let ally = new Alligator("Ally", "Green", "F", 200, 60, "big");
+let blly = new Beaver("Blly", "Green", "F", 200, 60, "big");
+let clly = new Cow("Clly", "Green", "F", 200, 60, "big");
+let dlly = new Deer("Dlly", "Green", "F", 200, 60, "big");
+let elly = new Eagle("Elly", "Green", "F", 200, 60, "big");
+let flly = new Falcon("Flly", "Green", "F", 200, 60, "big");
+let glly = new Giraffe("Glly", "Green", "F", 200, 60, "big");
+let hlly = new Hyena("Hlly", "Green", "F", 200, 60, "big");
+let illy = new Iguana("Illy", "Green", "F", 200, 60, "big");
+let jlly = new Jaguar("Jlly", "Green", "F", 200, 60, "big");
+let klly = new Kangaroo("Klly", "Green", "F", 200, 60, "big");
+let llly = new Leopard("Llly", "Green", "F", 200, 60, "big");
+let mlly = new Meerkat("Mlly", "Green", "F", 200, 60, "big");
+let nlly = new Newt("Nlly", "Green", "F", 200, 60, "big");
+let olly = new Okapi("Olly", "Green", "F", 200, 60, "big");
+let plly = new Penguin("Plly", "Green", "F", 200, 60, "big");
+let qlly = new Quail("Qlly", "Green", "F", 200, 60, "big");
+let rlly = new Racoon("Rlly", "Green", "F", 200, 60, "big");
+let slly = new Sloth("Slly", "Green", "F", 200, 60, "big");
+let tlly = new Tiger("Tlly", "Green", "F", 200, 60, "big");
+let ully = new Uguisu("Ully", "Green", "F", 200, 60, "big");
+let vlly = new Vulture("Vlly", "Green", "F", 200, 60, "big");
+let wlly = new Warthog("Wlly", "Green", "F", 200, 60, "big");
+let xlly = new XBeaver("Xlly", "Green", "F", 200, 60, "big");
+let ylly = new Yak("Ylly", "Green", "F", 200, 60, "big");
+let zlly = new Zebra("Zlly", "Green", "F", 200, 60, "big");
+
+
+let arr = []
+
+arr.push( ally)
+arr.push( blly)
+arr.push( clly)
+arr.push( dlly)
+arr.push( elly)
+arr.push( flly)
+arr.push( glly)
+arr.push( hlly)
+arr.push( illy)
+arr.push( jlly)
+arr.push( klly)
+arr.push( llly)
+arr.push( mlly)
+arr.push( nlly)
+arr.push( olly)
+arr.push( plly)
+arr.push( qlly)
+arr.push( rlly)
+arr.push( slly)
+arr.push( tlly)
+arr.push( ully)
+arr.push( vlly)
+arr.push( wlly)
+arr.push( xlly)
+arr.push( ylly)
+arr.push( zlly)
+
